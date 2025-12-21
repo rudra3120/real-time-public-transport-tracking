0a625ed54ab1f9099724773e0274d6a8d8948683
Rudra Pratap Singh <rudra31200@gmail.com>
2025-12-22 02:21:08 +0530

    Initial commit - Real-Time Public Transport Tracking System

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("✅ MySQL Connected Successfully");
+});
+
+module.exports = db;
